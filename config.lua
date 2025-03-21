6d2ab88ca3da941d88066d409f9ef380349b6477
Txgls <businesstoggled@gmail.com>
2025-03-21 17:41:52 +0000

    config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.WalletItem = {
+    name = 'wallet',          -- Item name (must match the item in ox_inventory/data/items.lua).
+    label = 'Wallet',         -- Item label.
+}
+
+-- Wallet stash settings
+Config.WalletStash = {
+    slots = 5,                -- Number of slots in the wallet.
+    weight = 100,           -- Maximum weight the wallet can hold (keep small).
+    label = "Wallet"          -- Label for the stash.
+}
+
+return Config
